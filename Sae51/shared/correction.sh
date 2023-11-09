814eeeb19dc27d97d985fda9e53c750a37295ad3
kims242 <philippek215@gmail.com>
2023-11-09 16:00:18 +0100

    update

---
+++ Sae51/shared/correction.sh
@@ -0,0 +1,3 @@
+sed s/settings?.headercolor/settings.headercolor/ /usr/local/lib/node_modules/@softwaretechnik/dbml-renderer/lib/renderer.js > tmp
+cp tmp  /usr/local/lib/node_modules/@softwaretechnik/dbml-renderer/lib/renderer.js
+rm tmp
--- Sae51/shared/dbml2sql.sh
+++ Sae51/shared/dbml2sql.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+#correction du fichier renderer.js
+./correction.sh
 for a in *.dbml
 do
     name=${a%.dbml}
     echo "processing $name"
     dbml2sql $a > $name.sql
+    dbml-renderer -i $a -o $name.svg
 done
