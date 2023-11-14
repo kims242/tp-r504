e1d081244e7d5cc7c93a2cf4716ea18d20538720
kims242 <philippek215@gmail.com>
2023-11-14 21:50:55 +0100

    up up up

---
+++ Sae51/sql/run_query.sh
@@ -0,0 +1,10 @@
+mysql -h 127.0.0.1 -u root -p'root' -P 3306 sae51db -e "select * from PC where PC_marque in (select id_marque from marque where nom_marque="hp");"
+mysql -h 127.0.0.1 -u root -p'root' -P 3306 sae51db -e "select * from PC where PC_marque in (select id_marque from marque where nom_marque="Asus");"
+mysql -h 127.0.0.1 -u root -p'root' -P 3306 sae51db -e "select * from PC where PC_marque in (select id_marque from marque where nom_marque="Dell");" 
+mysql -h 127.0.0.1 -u root -p'root' -P 3306 -e 
+mysql -h 127.0.0.1 -u root -p'root' -P 3306 -e 
+mysql -h 127.0.0.1 -u root -p'root' -P 3306 -e 
+mysql -h 127.0.0.1 -u root -p'root' -P 3306 -e 
+mysql -h 127.0.0.1 -u root -p'root' -P 3306 -e 
+mysql -h 127.0.0.1 -u root -p'root' -P 3306 -e 
+mysql -h 127.0.0.1 -u root -p'root' -P 3306 -e 
