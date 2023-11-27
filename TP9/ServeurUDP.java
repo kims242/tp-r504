a561d5fcecb43ce9c0db28f9dc5d54c5f59b0b1e
kims242 <philippek215@gmail.com>
2023-11-27 14:32:44 +0100

    update

--- TP9/ServeurUDP.java
+++ TP9/ServeurUDP.java
@@ -2,17 +2,16 @@ import java.io.*;
 import java.net.*;
 
 public class ServeurUDP {
-	public static void main(String[] args) {
-    	String s = "Bonjour";
-    	byte[] data = s.getBytes();
-
+	public static void main(String[] args) { // Ajout des arguments de la méthode main
     	try {
-        	InetAddress addr = InetAddress.getLocalHost();
-        	System.out.println("adresse=" + addr.getHostName());
-        	DatagramPacket packet = new DatagramPacket(data, data.length, addr, 1234);
-        	DatagramSocket sock = new DatagramSocket();
-        	sock.send(packet);
-        	sock.close();
+        	DatagramSocket sock = new DatagramSocket(1234);
+        	while (true) {
+            	System.out.println("-waiting data");
+            	DatagramPacket packet = new DatagramPacket(new byte[1024], 1024);
+            	sock.receive(packet);
+            	String str = new String(packet.getData() );
+            	System.out.println("str=" + str);
+        	}
     	} catch (Exception ex) {
         	System.out.println("erreur !");
         	ex.printStackTrace();
