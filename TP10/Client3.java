5eedd344ebfe4b83fecb1957ec5cb97517856884
kims242 <philippek215@gmail.com>
2024-02-07 15:45:13 +0100

    update 100

--- TP10/Client3.java
+++ TP10/Client3.java
@@ -11,7 +11,7 @@ public class Client3 {
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
             while (true) {
-                System.out.print("Entrez le titre du film (ou 'Fin' pour quitter) : ");
+                System.out.print("Entrez le titre d'un film : ");
                 String filmTitle = reader.readLine();
 
                 if ("stop".equalsIgnoreCase(filmTitle)) {
@@ -32,44 +32,55 @@ public class Client3 {
                         jsonReader.close();
                         isr.close();
 
-                        // Afficher les informations essentielles
-                        System.out.println("Date de sortie : " + jsonObject.getString("Released"));
-                        System.out.println("Acteurs principaux : " + jsonObject.getString("Actors"));
-						System.out.println( "duree=" + jsonObject.getString("Runtime"));
-						System.out.println("Genre : " + jsonObject.getString("Genre"));
-
-                        // Récupérer le tableau des critiques
-                        JsonArray ratingsArray = jsonObject.getJsonArray("Ratings");
-
-                        // Chercher le score de "Rotten Tomatoes"
-                        String rottenTomatoesScore = null;
-                        for (JsonValue rating : ratingsArray) {
-                            JsonObject ratingObject = (JsonObject) rating;
-                            if ("Rotten Tomatoes".equals(ratingObject.getString("Source"))) {
-                                rottenTomatoesScore = ratingObject.getString("Value");
-                                break;
-                            }
-                        }
-
-                        // Ajouter la mention en fonction du score
-                        if (rottenTomatoesScore != null) {
-                            int score = Integer.parseInt(rottenTomatoesScore.replaceAll("[^\\d]", ""));
-                            String mention;
-
-                            if (score < 20) {
-                                mention = "Nul";
-                            } else if (score >= 20 && score < 50) {
-                                mention = "Bof";
-                            } else if (score >= 50 && score < 70) {
-                                mention = "Bien";
-                            } else {
-                                mention = "Très bien";
-                            }
-
-                            System.out.println("Score Rotten Tomatoes : " + rottenTomatoesScore);
-                            System.out.println("Mention : " + mention);
+                        // Vérifier si le film a été trouvé
+                        if (jsonObject.containsKey("Error")) {
+                            System.out.println("Film non trouvé.");
                         } else {
-                            System.out.println("Score Rotten Tomatoes non trouvé.");
+                            // Afficher les informations essentielles
+                            System.out.println("Date de sortie : " + jsonObject.getString("Released"));
+                            System.out.println("Acteurs principaux : " + jsonObject.getString("Actors"));
+                            System.out.println("Durée : " + jsonObject.getString("Runtime"));
+                            System.out.println("Genre : " + jsonObject.getString("Genre"));
+
+                            // Récupérer le tableau des critiques
+                            JsonArray ratingsArray = jsonObject.getJsonArray("Ratings");
+
+                            // Chercher le score de "Rotten Tomatoes"
+                            String rottenTomatoesScore = null;
+                            for (JsonValue rating : ratingsArray) {
+                                JsonObject ratingObject = (JsonObject) rating;
+                                if ("Rotten Tomatoes".equals(ratingObject.getString("Source"))) {
+                                    rottenTomatoesScore = ratingObject.getString("Value");
+                                    break;
+                                }
+                            }
+
+							// Ajouter la mention en fonction du score
+						if (rottenTomatoesScore != null) {
+    					int score = Integer.parseInt(rottenTomatoesScore.replaceAll("[^\\d]", ""));
+    					String mention = "";
+
+    					if (score < 20) {
+        					mention = "Nul";
+    					}
+
+    					if (score >= 20 && score < 50) {
+       						 mention = "Bof";
+    					}
+
+    					if (score >= 50 && score < 70) {
+        					mention = "Bien";
+    					}
+
+    					if (score >= 70) {
+        					mention = "Très bien";
+    					}
+
+    						System.out.println("Score Rotten Tomatoes : " + rottenTomatoesScore);
+    						System.out.println("Mention : " + mention);
+						} else {
+    							System.out.println("Score Rotten Tomatoes non trouvé.");
+						}
                         }
                     } else {
                         System.out.println("Erreur HTTP : " + response.getStatusLine().getStatusCode());
